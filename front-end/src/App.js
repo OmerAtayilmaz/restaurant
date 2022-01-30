50695188201d3aa573cf4792d51034eac2be1c40
omer <omeratayilmaz@gmail.com>
2022-01-30 16:16:33 +0300

    frontend-3

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,16 @@
 import TopBanner from "./components/TopBanner";
 import Navbar from "./components/Navbar";
-import "./index.css";
 import NavbarDetail from "./components/NavbarDetail";
+import Main from "./components/Main";
+import "./index.css";
+
 function App() {
   return (
     <div className="App">
       <TopBanner />
       <Navbar />
       <NavbarDetail />
+      <Main />
     </div>
   );
 }
